60dd3d9388df20375729e7f123e11c789f4b5b20
DESKTOP-RLTFT9I\Shobhit <shobhit@capiot.com>
2017-10-09 18:51:41 +0530

    initial work

---
+++ projectSkeletons/templates/app.template.js
@@ -0,0 +1,97 @@
+module.exports = function(config){
+    var puttuRedis = config.puttuName?`const puttu = require("puttu-redis");
+const redis = require("redis");
+const client = redis.createClient();
+
+
+client.on("error", function (err) {
+    logger.error("Redis Disconnected, stopping service");
+    process.exit(0);
+});
+
+puttu.init(client);
+    `:"";
+    var puttuRegister = config.puttuName?`puttu.register("${config.projectName}", {
+        protocol: "http",
+        port: port,
+        api: "/${config.basePath}/"
+    }).catch(err => logger.error(err));
+`:"";
+    var appJs = `
+"use strict";
+const SwaggerExpress = require("swagger-express-mw");
+const app = require("express")();
+const cuti = require("cuti");
+const log4js = cuti.logger.getLogger;
+const logger = log4js.getLogger("${config.projectName}");
+const bluebird = require("bluebird");
+const Sequelize = require("sequelize");
+const pg = require('pg');
+
+global.Promise = bluebird;
+global.logger = logger;
+
+var dbName = "${config.db}";
+dbName = dbName.toLowerCase();
+var conStringPostgres = 'postgres://postgres:welcome@localhost:5432/postgres';
+var conStringDB = 'postgres://postgres:welcome@localhost:5432/' + dbName;
+
+var client = new pg.Client(conStringPostgres);
+try{
+  client.connect().then(()=>{
+	client.query("select count(*) from pg_catalog.pg_database where datname = '"+dbName+"'").then(result=>{
+		var dbCount = result.rows[0].count;
+		if(dbCount=="1"){
+			console.log("Database Exist");
+			registerSwagger();
+		}else{
+				client.query('CREATE DATABASE '+dbName).then(result=>{
+				registerSwagger();
+				});
+			}
+	});
+
+  });
+
+}catch(err){
+  console.log(err);
+}
+var counter = 0;
+var logMiddleware = (req, res, next) => {
+    var reqId = counter++;
+    if (reqId == Number.MAX_VALUE) {
+        reqId = counter = 0;
+    }
+
+    logger.info(reqId + " " + req.ip + " " +  req.method + " " + req.originalUrl);
+    next();
+    logger.trace(reqId + " Sending Response");
+};
+app.use(logMiddleware);
+
+var config = {
+    appRoot: __dirname
+};
+module.exports = app;
+
+
+function registerSwagger(){
+  SwaggerExpress.create(config, function(err, swaggerExpress) {
+      if (err) { throw err; }
+
+      swaggerExpress.register(app);
+
+      var port = process.env.PORT || ${config.port};
+      app.listen(port, (err) => {
+          if(!err){
+              ${puttuRegister}
+              logger.info("Server started on port "+port);
+          }
+          else
+              logger.error(err);
+      });
+
+  });
+}    `;
+    return appJs;
+};
---
+++ projectSkeletons/templates/controller.main.template.js
@@ -0,0 +1,45 @@
+module.exports = function(name, config){
+    var configFile = config.configFileData
+    var collectionName = configFile.collectionName;
+    var modelName = configFile.modelName;
+    var controllerJs = `"use strict";
+
+const definition = require("../helpers/${name}.definition.js").definition;
+const SMCrud = require("swagger-sequelize-crud");
+const cuti = require("cuti");
+const Sequelize = require("sequelize");
+const logger = global.logger;
+var dbName="${config.db}";
+dbName=dbName.toLowerCase();
+var options = {
+    logger:logger,
+    collectionName:"${collectionName}"
+};
+//database wide options
+var opts = {
+    define: {
+        //prevent sequelize from pluralizing table names
+        freezeTableName: true
+    }
+}
+var sequelize = new Sequelize("postgres://postgres:welcome@localhost:5432/"+dbName,opts);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+var crudder = new SMCrud(sequelize, definition ,"${modelName}", options);
+module.exports = {
+    create:crudder.create,
+    index:crudder.index,
+    show:crudder.show,
+    destroy:crudder.markAsDeleted,
+    update:crudder.update
+};
+    `;
+    return controllerJs;
+};
